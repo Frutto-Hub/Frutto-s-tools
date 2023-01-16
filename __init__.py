65518a190a07e4c2f27486ec924caff4d0edc143
Frutto <agent4752@yandex.ru>
2023-01-16 18:21:21 +0300

    Added pannels and and changed behavior

--- __init__.py
+++ __init__.py
@@ -20,101 +20,100 @@ from bpy.utils import register_class, unregister_class
 from bpy.props import PointerProperty, FloatProperty, IntProperty, StringProperty, BoolProperty, EnumProperty
 
 import datetime
+from math import pi
 
 
 # custom ft props --------------------------------------------------------------------------------------------------
 class FT_scene_props(PropertyGroup):
-    auto_smooth_angle: FloatProperty(
-        name='Auto smooth angle',
+    remove_doubles_enabled: BoolProperty(
+        name='Remove double vertices',
+        description='Remove double vertices (Remove vertices same coordinates)',
+        default=True,
+    )
+    merge_distance: FloatProperty(
+        name='Merge distance',
+        default=0.0001,
+        min=0,
+        max=1,
+    )
+    to_quads_enabled: BoolProperty(
+        name='Triangles to quads',
+        description='Change triangles to quads',
+        default=True,
+    )
+    max_face_angle: FloatProperty(
+        name='Max face angle',
+        default=40 * (pi / 180),
+        min=0,
+        max=180 * (pi / 180),
+        subtype='ANGLE'
+    )
+    max_shape_angle: FloatProperty(
+        name='Max shape angle',
+        default=40 * (pi / 180),
+        min=0,
+        max=180 * (pi / 180),
+        subtype='ANGLE'
+    )
+    shade_smooth_enabled: BoolProperty(
+        name='Shade smooth',
+        description='Make shade smooth',
+        default=True,
+    )
+
+    smooth_angle: FloatProperty(
+        name='Smooth angle',
         default=1.0472,
         min=0,
-        max=3.14159,
+        max=180 * (pi / 180),
         subtype='ANGLE'
     )
 
-    database_folder: StringProperty(
-        name="Database folder set",
-        description='Real seted folder for storing all data required for dbdmi addon',
-        default='',
-        subtype='FILE_PATH'
-    )
-    pak_folder: StringProperty(
-        name="DBD Pak folder set",
-        description='DBD Pak files location on your computer',
-        default='',
-        subtype='FILE_PATH'
-    )
-
-    select_import_character_role: EnumProperty(
-        name="Select character role",
-        items=(('survivour', 'Survivour', 'Select survivour role'),
-               ('killer', 'Killer', 'Select killer role')),
-        description='Select survivour or killer role',
-        default='survivour'
-    )
-
-    select_import_character_sex: EnumProperty(
-        name="Select survivour sex",
-        items=(('male', 'Male', 'Select male character'),
-               ('female', 'Female', 'Select female character')),
-        description='Select male or female character',
-        default='male'
-    )
-
-    filter_by_sex: BoolProperty(
-        name='Filter by sex',
-        description='Filter characters by sex for icons preview',
+    apply_loc_rot_scale: BoolProperty(
+        name='Apply LocRocScale after translation',
+        description='Apply LocRocScale after translation',
         default=True,
     )
 
 
 # operators ------------------------------------------------------------------------------------------------------------
-class FT_OT_Test(Operator):
-    bl_idname = "dbdmi.test"  # important since its how bpy.ops.import_test.some_data is constructed
-    bl_label = "Test opearator"
 
-    def execute(self, context):
-        print(bpy.path.abspath(path='', start='//'))
-        print('[DBDMI]: Test operator executed')
-        self.report({'INFO'}, 'Test operator executed')
-        return {'FINISHED'}
-
-
-class FT_OT_optimise_mesh(Operator):
+class FT_OT_mesh_optimisation(Operator):
     bl_idname = "ft.optimise_mesh"  # important since its how bpy.ops.import_test.some_data is constructed
     bl_label = "Optimise Mesh"
 
     def execute(self, context):
+        props = context.scene.ft_props
         selected_mesh = [obj for obj in context.selected_objects if obj.type == 'MESH']
         if selected_mesh:
-            # active object MUST be MESH type, otherwise an exception occurs (context is incorrect)
+            # active object MUST be 'MESH' type, otherwise an exception occurs (context is incorrect)
             # ask a question to somebody, why this happens???
             context.view_layer.objects.active = selected_mesh[0]
         else:
             self.report({'WARNING'}, 'Nothing to optimise')
             return {'CANCELLED'}
 
+        # think how to optimise this
         bpy.ops.object.select_all(action='DESELECT')
         for objects_optimised, obj in enumerate(selected_mesh, 1):
-            print(obj.type)
-            # print(f'{i}) obj_name = {obj.name}, obj_type = {obj.type}')
-            bpy.ops.object.mode_set(mode='EDIT')
+            if props.remove_doubles_enabled:
+                bpy.ops.object.mode_set(mode='EDIT')
+                bpy.ops.mesh.select_all(action='SELECT')
+                bpy.ops.mesh.remove_doubles(threshold=props.merge_distance)
+                bpy.ops.mesh.select_all(action='DESELECT')
+                bpy.ops.object.editmode_toggle()
 
-            bpy.ops.mesh.select_all(action='SELECT')
+            if props.to_quads_enabled:
+                bpy.ops.object.mode_set(mode='EDIT')
+                bpy.ops.mesh.select_all(action='SELECT')
+                bpy.ops.mesh.tris_convert_to_quads(uvs=True)
+                bpy.ops.mesh.select_all(action='DESELECT')
+                bpy.ops.object.editmode_toggle()
 
-            bpy.ops.mesh.remove_doubles(threshold=0.0001)
-
-            bpy.ops.mesh.tris_convert_to_quads(uvs=True)
-
-            bpy.ops.mesh.select_all(action='DESELECT')
-
-            bpy.ops.object.editmode_toggle()
-
-            obj.select_set(True)
-
-            bpy.ops.object.shade_smooth(use_auto_smooth=True)
-
-            obj.select_set(False)
+            if props.shade_smooth_enabled:
+                obj.select_set(True)
+                bpy.ops.object.shade_smooth(use_auto_smooth=True)
+                obj.select_set(False)
 
         # selecting back mesh objects
         for obj in selected_mesh:
@@ -126,6 +125,58 @@ class FT_OT_optimise_mesh(Operator):
             self.report({'INFO'}, f'{objects_optimised} mesh objects successfully optimised')
         print('[DBDMI]: Mesh optimised executed')
 
+        return {'FINISHED'}
+
+
+class FT_OT_reset_opt_params(Operator):
+    bl_idname = "ft.reset_opt_params"
+    bl_label = "Reset optimisation parameters"
+
+    def execute(self, context):
+        props = context.scene.ft_props
+        props.remove_doubles_enabled = True
+        props.merge_distance = 0.0001
+        props.to_quads_enabled = True
+        props.max_face_angle = 40 * (pi / 180)
+        props.max_shape_angle = 40 * (pi / 180)
+        props.shade_smooth_enabled = True
+        props.smooth_angle = 1.0472
+
+        self.report({'INFO'}, 'Optimisation parameters reset')
+        return {'FINISHED'}
+
+
+class FT_OT_loc_rot_scale_to_active_object(Operator):
+    bl_idname = "ft.loc_rot_scale_to_active_object"
+    bl_label = "Translate to active object"
+
+    def execute(self, context):
+        props = context.scene.ft_props
+        active_obj = context.active_object
+        selected_objects = context.selected_objects
+        selected_objects_without_active = set(selected_objects).difference({active_obj})
+        print(f'source_obj = {selected_objects_without_active}')
+        print(f'active_obj = {active_obj}')
+
+        ## debug, doesn't work
+        for ob in selected_objects_without_active:
+            ob.location = active_obj.location
+            ob.rotation_euler = active_obj.rotation_euler
+            ob.scale = active_obj.scale
+            if props.apply_loc_rot_scale:
+                bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+
+        self.report({'INFO'}, 'Loc Rot Scale transferred to active object')
+        return {'FINISHED'}
+
+
+class FT_OT_Test(Operator):
+    bl_idname = "ft.test"  # important since its how bpy.ops.import_test.some_data is constructed
+    bl_label = "Test opearator"
+
+    def execute(self, context):
+        print('[DBDMI]: Test operator executed')
+        self.report({'INFO'}, 'Test operator executed')
         return {'FINISHED'}
 
 
@@ -142,37 +193,6 @@ class FT_OT_time_update(Operator):
         return {'FINISHED'}
 
 
-class FT_OT_loc_rot_scale_to_active_object(Operator):
-    bl_idname = "ft.loc_rot_scale_to_active_object"
-    bl_label = "Loc Rot Scale to active object"
-
-    def execute(self, context):
-        selected_objs = context.selected_objects
-
-        # check, that exactly two object has beed selected
-        if len(selected_objs) < 2:
-            self.report({'WARNING'}, 'Not enough objects selected')
-            return {'CANCELLED'}
-        elif len(selected_objs) > 2:
-            self.report({'WARNING'}, 'Too many objects selected')
-            return {'CANCELLED'}
-
-        target_obj = context.active_object
-        selected_objs = context.selected_objects
-        # my_list = [active_obj]
-        source_obj = set(selected_objs).difference(set([target_obj]))
-        print(f'source_obj = {source_obj}')
-        print(f'target_obj = {target_obj}')
-
-        for ob in source_obj:
-            ob.location = target_obj.location
-            ob.rotation_euler = target_obj.rotation_euler
-            ob.scale = target_obj.scale
-
-        self.report({'INFO'}, 'Loc Rot Scale transferred to active object')
-        return {'FINISHED'}
-
-
 # operators end
 
 
@@ -183,49 +203,68 @@ class FT_common_panel:
     bl_category = 'FT'
 
 
-class FT_PT_mesh_optimise(FT_common_panel, Panel):
+class FT_PT_mesh_optimisation(FT_common_panel, Panel):
     bl_label = 'Mesh optimisation'
 
     def draw(self, context):
         layout = self.layout
         props = context.scene.ft_props
 
-        row = layout.row(align=True)
-        row.label(text='Select character:')
-        row.prop(props, 'filter_by_sex')
-
         row = layout.row()
-        row.prop(props, 'select_import_character_role',
-                 expand=True,
-                 )
-        if props.filter_by_sex:
+        row.prop(props, 'remove_doubles_enabled')
+
+        if props.remove_doubles_enabled:
             row = layout.row()
-            row.prop(props, 'select_import_character_sex',
-                     expand=True,
-                     )
+            row.prop(props, 'merge_distance')
 
         row = layout.row()
+        row.prop(props, 'to_quads_enabled')
+
+        if props.to_quads_enabled:
+            row = layout.row()
+            row.prop(props, 'max_face_angle')
+
+            row = layout.row()
+            row.prop(props, 'max_shape_angle')
 
         row = layout.row()
+        row.prop(props, 'shade_smooth_enabled')
+
+        if props.shade_smooth_enabled:
+            row = layout.row()
+            row.prop(props, 'smooth_angle')
+
+        row = layout.row()
+        row.label(text=f'Mesh objects selected: {len([obj for obj in context.selected_objects if obj.type == "MESH"])}')
+        row.operator('ft.reset_opt_params')
+
+        row = layout.row()
+        row.scale_y = 1.5
         row.operator('ft.optimise_mesh')
 
-        row = layout.row()
-        row.operator('ft.test_time')
+
+class FT_PT_mesh_translation(FT_common_panel, Panel):
+    bl_label = 'Object translation'
+
+    def draw(self, context):
+        props = context.scene.ft_props
+        layout = self.layout
 
         row = layout.row()
-        row.label(text=f'Last updated: {props.last_database_updated}', icon='TEMP')
+        row.label(text='Change LocRotScale of selected objects to active object')
+
+        row = layout.row()
+        row.label(text=f'Active object: {context.active_object.name}')
+
+        row = layout.row()
+        obj_count = 0 if len(context.selected_objects) == 0 else len(context.selected_objects) - 1
+        row.label(text=f'Objects to translate: {obj_count}')
+
+        row = layout.row()
+        row.prop(props, 'apply_loc_rot_scale')
 
         row = layout.row()
         row.operator('ft.loc_rot_scale_to_active_object')
-
-
-class FT_PT_mesh_moving(FT_common_panel, Panel):
-    bl_label = 'Mesh translation'
-
-    def draw(self, context):
-        layout = self.layout
-        row = layout.row()
-        row.label(text='Settings panel')
 
 
 class FT_PT_test(FT_common_panel, Panel):
@@ -235,28 +274,22 @@ class FT_PT_test(FT_common_panel, Panel):
         layout = self.layout
         props = context.scene.ft_props
 
-
-class FT_PT_info(FT_common_panel, Panel):
-    bl_label = 'Info'
-
-    def draw(self, context):
-        layout = self.layout
         row = layout.row()
-        row.label(text='Info panel for FT')
+        row.operator('ft.test_time')
 
 
 # panels end
 
 classes = [
     FT_scene_props,
-    FT_OT_optimise_mesh,
+    FT_OT_mesh_optimisation,
+    FT_OT_reset_opt_params,
+    FT_OT_loc_rot_scale_to_active_object,
     FT_OT_Test,
-
-    FT_PT_info,
-    FT_PT_mesh_optimise,
-    FT_PT_mesh_moving,
+    FT_OT_time_update,
+    FT_PT_mesh_optimisation,
+    FT_PT_mesh_translation,
     FT_PT_test,
-
 ]
 
 
